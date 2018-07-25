c31ab71fe9a2890a018cdef4e355d63554b4cf53
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-25 14:54:39 -0600

    Kat's Test

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,71 @@
 class Pantry
+  attr_reader :stock,
+              :shopping_list,
+              :cookbook
 
+  def initialize
+    @stock = {}
+    @shopping_list = {}
+    @cookbook = []
+  end
+
+  def stock_check(item)
+    if @stock[item]
+      @stock[item]
+    else
+      0
+    end
+  end
+
+  def restock(item, quantity)
+    if @stock[item]
+      @stock[item] += quantity
+    else
+      @stock[item] = quantity
+    end
+  end
+
+  def add_to_shopping_list(recipe)
+    recipe.ingredients.each do |item, quantity|
+      if @shopping_list[item]
+        @shopping_list[item] += quantity
+      else
+        @shopping_list[item] = quantity
+      end
+    end
+  end
+
+  def print_shopping_list
+    list = @shopping_list.inject("") do |memo, item_quantity|
+      memo += "* #{item_quantity[0]}: #{item_quantity[1]}\n"
+    end.strip
+  end
+
+  def add_to_cookbook(recipe)
+    @cookbook << recipe
+  end
+
+  def what_can_i_make
+    possible_recipes.map do |recipe|
+      recipe.name
+    end
+  end
+
+  def possible_recipes
+  @cookbook.find_all do |recipe|
+    recipe.ingredients.all? do |item, quantity|
+      @stock[item] >= quantity
+      end
+    end
+  end
+
+  def how_many_can_i_make
+    # don't know...
+    can_make = possible_recipes.map do |recipe|
+      recipe.ingredients.map do |item, quantity|
+        @stock[item] / quantity
+      end.min
+    end
+
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,162 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 
+  def test_it_starts_empty
+    pantry = Pantry.new
+    expected = {}
+
+    assert_equal expected, pantry.stock
+  end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock_items
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    assert_equal 10, pantry.stock_check("Cheese")
+
+    pantry.restock("Cheese", 20)
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_add_ingredients_to_shopping_list
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    r.add_ingredient("Cheese", 20)
+    r.add_ingredient("Flour", 20)
+
+    expected = {}
+    assert_equal expected, pantry.shopping_list
+    pantry.add_to_shopping_list(r)
+    expected = {"Cheese" => 20, "Flour" => 20}
+    assert_equal expected, pantry.shopping_list
+  end
+
+  def test_it_can_add_more_recipes
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    r.add_ingredient("Cheese", 20)
+    r.add_ingredient("Flour", 20)
+    pantry.add_to_shopping_list(r)
+    recipe_2 = Recipe.new("Spaghetti")
+    recipe_2.add_ingredient("Spaghetti Noodles", 10)
+    recipe_2.add_ingredient("Marinara Sauce", 10)
+    recipe_2.add_ingredient("Cheese", 5)
+    pantry.add_to_shopping_list(recipe_2)
+
+    expected = {"Cheese" => 25, "Flour" => 20, "Spaghetti Noodles" => 10, "Marinara Sauce" => 10}
+    assert_equal expected, pantry.shopping_list
+  end
+
+  def test_it_prints_shopping_list
+    pantry = Pantry.new
+    r = Recipe.new("Cheese Pizza")
+    r.add_ingredient("Cheese", 20)
+    r.add_ingredient("Flour", 20)
+    pantry.add_to_shopping_list(r)
+    recipe_2 = Recipe.new("Spaghetti")
+    recipe_2.add_ingredient("Spaghetti Noodles", 10)
+    recipe_2.add_ingredient("Marinara Sauce", 10)
+    recipe_2.add_ingredient("Cheese", 5)
+    pantry.add_to_shopping_list(recipe_2)
+    expected = "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10"
+
+    assert_equal expected, pantry.print_shopping_list
+  end
+
+  def test_it_can_add_recipes_to_cookbook
+    pantry = Pantry.new
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    assert_equal [], pantry.cookbook
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    assert_equal [r1, r2, r3], pantry.cookbook
+  end
+
+  def test_it_tells_what_recipes_can_be_made
+    pantry = Pantry.new
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Cucumbers", 120)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    expected = ["Pickles", "Peanuts"]
+    assert_equal expected, pantry.what_can_i_make
+  end
+
+  def test_it_tells_how_many_can_be_made
+    pantry = Pantry.new
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Cucumbers", 120)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    expected = {"Pickles" => 4, "Peanuts" => 2}
+    assert_equal expected, pantry.how_many_can_i_make
+  end
 end
